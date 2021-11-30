2970103cab43fb4ea71a9b5e87a0003ce5db9835
Dion Laranjeira <dionribeiro.rr@gmail.com>
2021-11-30 09:32:58 -0400

    Request json API - Teste

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
 
+#dependencias
+import requests
+
 app = Flask(__name__)
 
+api_key = '2a149d289a2eeb9716e157bd1448dfb3'
+
+def openWeather(cidade, api_key):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={cidade}&APPID={api_key}'
+    response = requests.get(url).json()
+    print(response)
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():  # put application's code here
+    openWeather("Boa Vista,BR",api_key)
     return 'Bem vindo a API - Previsão do Tempo Linx'
-
 
 if __name__ == '__main__':
     app.run()
